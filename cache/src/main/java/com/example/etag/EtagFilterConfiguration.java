8c20100b080f6cf69ffa469d249f5b3a61974e9f
leokim <kunga077@gmail.com>
2022-09-02 22:06:37 +0900

    feat: 캐시 무효화

--- cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
+++ cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
@@ -2,6 +2,8 @@ package com.example.etag;
 
 import static com.example.version.CacheBustingWebConfig.PREFIX_STATIC_RESOURCES;
 
+import com.example.version.ResourceVersion;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,12 +12,15 @@ import org.springframework.web.filter.ShallowEtagHeaderFilter;
 @Configuration
 public class EtagFilterConfiguration {
 
+    @Autowired
+    private ResourceVersion version;
+
     @Bean
     public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
         FilterRegistrationBean<ShallowEtagHeaderFilter> filterRegistrationBean = new FilterRegistrationBean<>(
                 new ShallowEtagHeaderFilter());
         filterRegistrationBean
-                .addUrlPatterns("/etag");
+                .addUrlPatterns("/etag", PREFIX_STATIC_RESOURCES + "/*");
         return filterRegistrationBean;
     }
 }
