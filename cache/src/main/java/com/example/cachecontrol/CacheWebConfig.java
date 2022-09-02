c686827733e7a1389ce50acbc7484054f6d385d8
hyeonic <dev.hyeonic@gmail.com>
2022-09-02 13:51:16 +0900

    feat: 휴리스틱 캐시 방지를 위한 interceptor 추가

--- cache/src/main/java/com/example/cachecontrol/CacheWebConfig.java
+++ cache/src/main/java/com/example/cachecontrol/CacheWebConfig.java
@@ -1,13 +1,24 @@
 package com.example.cachecontrol;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CacheWebConfig implements WebMvcConfigurer {
 
+    private final HandlerInterceptor cacheInterceptor;
+
+    public CacheWebConfig(final HandlerInterceptor cacheInterceptor) {
+        this.cacheInterceptor = cacheInterceptor;
+    }
+
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(cacheInterceptor)
+                .addPathPatterns("/**");
     }
 }
