0499b264cfb941ea333343450205598f8fa150b1
syoun602 <syoun602@gmail.com>
2022-09-02 17:10:21 +0900

    feat: 인터셉터를 통해 index 접근시 cache control header 추가

---
+++ cache/src/main/java/com/example/cachecontrol/CacheInterceptor.java
@@ -0,0 +1,24 @@
+package com.example.cachecontrol;
+
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class CacheInterceptor implements HandlerInterceptor {
+
+    @Override
+    public void afterCompletion(final HttpServletRequest request,
+                                final HttpServletResponse response,
+                                final Object handler,
+                                final Exception ex) {
+        final String cacheControl = CacheControl
+                .noCache()
+                .cachePrivate()
+                .getHeaderValue();
+        response.addHeader(HttpHeaders.CACHE_CONTROL, cacheControl);
+    }
+}
--- cache/src/main/java/com/example/cachecontrol/CacheWebConfig.java
+++ cache/src/main/java/com/example/cachecontrol/CacheWebConfig.java
@@ -1,13 +1,22 @@
 package com.example.cachecontrol;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.util.List;
 
 @Configuration
 public class CacheWebConfig implements WebMvcConfigurer {
 
+    private final List<HandlerInterceptor> handlerInterceptors;
+
+    public CacheWebConfig(final List<HandlerInterceptor> handlerInterceptors) {
+        this.handlerInterceptors = handlerInterceptors;
+    }
+
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
+        handlerInterceptors.forEach(registry::addInterceptor);
     }
 }
