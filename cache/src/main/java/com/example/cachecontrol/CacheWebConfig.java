4effa822d64fd648b6125e2d9032eb5e7bccdda6
seong-wooo <seongwoo0513@uos.ac.kr>
2022-09-02 12:21:59 +0900

    feat: 휴리스틱 캐싱 제거

--- cache/src/main/java/com/example/cachecontrol/CacheWebConfig.java
+++ cache/src/main/java/com/example/cachecontrol/CacheWebConfig.java
@@ -7,7 +7,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CacheWebConfig implements WebMvcConfigurer {
 
+    private final CacheControllInterceptor cacheControllInterceptor;
+
+    public CacheWebConfig(CacheControllInterceptor cacheControllInterceptor) {
+        this.cacheControllInterceptor = cacheControllInterceptor;
+    }
+
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(cacheControllInterceptor)
+                .addPathPatterns("/**");
     }
 }
