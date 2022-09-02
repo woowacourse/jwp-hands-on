ac7269116a5f49a90622486eccda896bd4e20ca0
woong7 <driedseas@gmail.com>
2022-09-02 15:46:53 +0900

    feat : 캐시 무효화

--- cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
+++ cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
@@ -1,18 +1,29 @@
 package com.example.etag;
 
+import static com.example.version.CacheBustingWebConfig.*;
+
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
+import com.example.version.ResourceVersion;
+
 @Configuration
 public class EtagFilterConfiguration {
+
+    private final ResourceVersion version;
+
+    public EtagFilterConfiguration(final ResourceVersion version) {
+        this.version = version;
+    }
 
     @Bean
     public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
         FilterRegistrationBean<ShallowEtagHeaderFilter> bean = new FilterRegistrationBean<>();
         bean.setFilter(new ShallowEtagHeaderFilter());
-        bean.addUrlPatterns("/etag");
+        bean.addUrlPatterns("/etag",
+            String.format("%s/%s%s", PREFIX_STATIC_RESOURCES, version.getVersion(), "/js/index.js"));
         return bean;
     }
 }
