0ad5c17725e14ea265f0e568c3ea27eaaf285af6
seong-wooo <seongwoo0513@uos.ac.kr>
2022-09-02 14:04:21 +0900

    feat: 캐시 무효화 적용

--- cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
+++ cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
@@ -1,5 +1,8 @@
 package com.example.etag;
 
+import static com.example.version.CacheBustingWebConfig.PREFIX_STATIC_RESOURCES;
+
+import com.example.version.ResourceVersion;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,11 +11,17 @@ import org.springframework.web.filter.ShallowEtagHeaderFilter;
 @Configuration
 public class EtagFilterConfiguration {
 
+    private final ResourceVersion version;
+
+    public EtagFilterConfiguration(ResourceVersion version) {
+        this.version = version;
+    }
+
     @Bean
     public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
         FilterRegistrationBean<ShallowEtagHeaderFilter> filterRegistrationBean
                 = new FilterRegistrationBean<>(new ShallowEtagHeaderFilter());
-        filterRegistrationBean.addUrlPatterns("/etag");
+        filterRegistrationBean.addUrlPatterns("/etag", String.format("%s/%s/js/index.js", PREFIX_STATIC_RESOURCES, version.getVersion()));
         return filterRegistrationBean;
     }
 }
