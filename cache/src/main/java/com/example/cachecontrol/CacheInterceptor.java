c25328747e676712c472c35c472a54471bc52c0f
dongho <dongho108@naver.com>
2022-09-02 11:32:58 +0900

    feat: 모든 응답에 no-cache, private 추가

---
+++ cache/src/main/java/com/example/cachecontrol/CacheInterceptor.java
@@ -0,0 +1,19 @@
+package com.example.cachecontrol;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class CacheInterceptor implements HandlerInterceptor {
+
+    @Override
+    public void postHandle(final HttpServletRequest request,
+                           final HttpServletResponse response,
+                           final Object handler,
+                           final ModelAndView modelAndView) {
+        response.addHeader("Cache-Control", "no-cache, private");
+    }
+}
