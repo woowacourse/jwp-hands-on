1d034adbc2ebc327f17cd1b13d0e728edfe35fcd
Gyuchool <balance0591@naver.com>
2022-09-02 20:34:12 +0900

    feat: delete heuristrics caching

---
+++ cache/src/main/java/com/example/cachecontrol/CacheInterceptor.java
@@ -0,0 +1,18 @@
+package com.example.cachecontrol;
+
+import com.google.common.net.HttpHeaders;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.CacheControl;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class CacheInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        response.addHeader(HttpHeaders.CACHE_CONTROL, CacheControl.noCache().cachePrivate().getHeaderValue());
+        return true;
+    }
+}
