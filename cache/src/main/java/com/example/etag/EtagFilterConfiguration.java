751d2677e83bc8d8adc5ce9d023943f503be46a7
YJGwon <yj970125@gmail.com>
2022-09-02 17:41:49 +0900

    feat: 정적 리소스 Cache Busting 적용

--- cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
+++ cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
@@ -1,5 +1,9 @@
 package com.example.etag;
 
+import static com.example.version.CacheBustingWebConfig.PREFIX_STATIC_RESOURCES;
+
+import com.example.version.ResourceVersion;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,11 +12,18 @@ import org.springframework.web.filter.ShallowEtagHeaderFilter;
 @Configuration
 public class EtagFilterConfiguration {
 
+    private final ResourceVersion version;
+
+    @Autowired
+    public EtagFilterConfiguration(final ResourceVersion version) {
+        this.version = version;
+    }
+
     @Bean
     public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
         final FilterRegistrationBean<ShallowEtagHeaderFilter> registration = new FilterRegistrationBean<>();
         registration.setFilter(new ShallowEtagHeaderFilter());
-        registration.addUrlPatterns("/etag");
+        registration.addUrlPatterns("/etag", PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/*");
         return registration;
     }
 }
