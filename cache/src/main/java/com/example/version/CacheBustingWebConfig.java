daa98232b6532f71d65d414411eff1f027a7407a
asebn1 <asebn121@gmail.com>
2022-09-02 15:35:50 +0900

    feat: 3단계 캐시 무효화(Cache Busting) 적용

--- cache/src/main/java/com/example/version/CacheBustingWebConfig.java
+++ cache/src/main/java/com/example/version/CacheBustingWebConfig.java
@@ -1,9 +1,15 @@
 package com.example.version;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.Duration;
 
 @Configuration
 public class CacheBustingWebConfig implements WebMvcConfigurer {
@@ -20,6 +26,7 @@ public class CacheBustingWebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         registry.addResourceHandler(PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/**")
+                .setCacheControl(CacheControl.maxAge(Duration.ofDays(365)).cachePublic())
                 .addResourceLocations("classpath:/static/");
     }
 }
