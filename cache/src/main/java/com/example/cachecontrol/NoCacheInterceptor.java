437980029cc31fd4c63158e8d278e12a74d3f4dc
sure-why-not <pkse1213@gmail.com>
2022-09-02 21:53:41 +0900

    feat: 응답 헤더에 Cache-Control: no-cache, private 적용

---
+++ cache/src/main/java/com/example/cachecontrol/NoCacheInterceptor.java
@@ -0,0 +1,18 @@
+package com.example.cachecontrol;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class NoCacheInterceptor implements HandlerInterceptor {
+
+    @Override
+    public void postHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler,
+                           final ModelAndView modelAndView) throws Exception {
+        CacheControl cacheControl = CacheControl.noCache().cachePrivate();
+        response.setHeader(HttpHeaders.CACHE_CONTROL, cacheControl.getHeaderValue());
+    }
+}
