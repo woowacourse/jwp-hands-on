43ef768c388a304a9da167a827fa6918d7798376
kang-hyungu <hkkang@woowahan.com>
2022-09-02 17:15:13 +0900

    cache solution

--- cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
+++ cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
@@ -1,12 +1,25 @@
 package com.example.etag;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+import static com.example.version.CacheBustingWebConfig.PREFIX_STATIC_RESOURCES;
 
 @Configuration
 public class EtagFilterConfiguration {
 
-//    @Bean
-//    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
-//        return null;
-//    }
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+        final FilterRegistrationBean<ShallowEtagHeaderFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new ShallowEtagHeaderFilter());
+        registration.addUrlPatterns(
+                "/etag",
+
+                // js, css 같은 정적 파일에 ETag를 적용하기 위해 경로 추가
+                PREFIX_STATIC_RESOURCES + "/*"
+        );
+        return registration;
+    }
 }
--- cache/src/main/java/com/example/version/CacheBustingWebConfig.java
+++ cache/src/main/java/com/example/version/CacheBustingWebConfig.java
@@ -2,8 +2,11 @@ package com.example.version;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.Duration;
 
 @Configuration
 public class CacheBustingWebConfig implements WebMvcConfigurer {
@@ -20,6 +23,13 @@ public class CacheBustingWebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         registry.addResourceHandler(PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/**")
-                .addResourceLocations("classpath:/static/");
+                .addResourceLocations("classpath:/static/")
+
+                // ETag를 사용하니까 Last-Modified는 불필요하지 않을까?
+                // Last-Modified를 추가하면 캐싱 외에도 크롤러에게 마지막 수정 시간을 알려주어 크롤링 빈도를 조정할 수 있다.
+                // 자세한 내용은 아래 링크를 참고하자.
+                // https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching#sect1
+                .setUseLastModified(true)
+                .setCacheControl(CacheControl.maxAge(Duration.ofDays(365)).cachePublic());
     }
 }
