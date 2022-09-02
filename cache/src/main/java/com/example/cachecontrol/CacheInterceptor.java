a94256edaced703351b15b000067062518a7761d
Juhyung990122 <harukawa99@naver.com>
2022-09-02 15:31:09 +0900

    feat: 휴리스틱 캐싱 제거하기

---
+++ cache/src/main/java/com/example/cachecontrol/CacheInterceptor.java
@@ -0,0 +1,19 @@
+package com.example.cachecontrol;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class CacheInterceptor implements HandlerInterceptor {
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+
+        final String cacheControl = CacheControl.noCache().cachePrivate().getHeaderValue();
+        response.setHeader(HttpHeaders.CACHE_CONTROL, cacheControl);
+    }
+}
