f80539f7d26f63da03237fde7d8a5489a0a2f17e
pup-paw <wldusdl0310@gmail.com>
2022-09-04 23:47:09 +0900

    feat: 휴리스틱 캐싱 제거하기

--- cache/src/main/java/com/example/cachecontrol/CacheWebConfig.java
+++ cache/src/main/java/com/example/cachecontrol/CacheWebConfig.java
@@ -1,13 +1,18 @@
 package com.example.cachecontrol;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
 
 @Configuration
 public class CacheWebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
+        WebContentInterceptor interceptor = new WebContentInterceptor();
+        interceptor.addCacheMapping(CacheControl.noCache().cachePrivate(), "/*");
+        registry.addInterceptor(interceptor);
     }
 }
