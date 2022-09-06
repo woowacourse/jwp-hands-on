238029058eea589b5fc8bcbae409617f243d9053
ash <54317630+dongho108@users.noreply.github.com>
2022-09-06 10:35:19 +0900

    HTTP 캐시 실습 (#1)
    * feat: 모든 응답에 no-cache, private 추가
    
    * feat: 압축 기능 추가
    
    * feat: `/etag`로 들어오는 요청의 응답에 etag 추가
    
    * feat: 정적 리소스 파일 캐시 버스팅 설정

--- cache/src/main/java/com/example/GreetingController.java
+++ cache/src/main/java/com/example/GreetingController.java
@@ -1,11 +1,10 @@
 package com.example;
 
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class GreetingController {
