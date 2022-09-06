4ab3280d5bca605b6573462f5df5c6cd43bb6300
Na Ilseong <ilseongdev@gmail.com>
2022-09-06 17:11:18 +0900

    Cache (#1)
    * feat: '/'응답에 No-Cache, private 적용
    
    * chore: HTTP 응답 압축 설정
    
    * feat: ETag/If-None-Match 적용
    
    * feat: 캐시 무효화

--- cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
+++ cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
@@ -1,12 +1,25 @@
 package com.example.etag;
 
+import com.example.version.ResourceVersion;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @Configuration
 public class EtagFilterConfiguration {
 
-//    @Bean
-//    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
-//        return null;
-//    }
+    private final ResourceVersion version;
+
+    public EtagFilterConfiguration(final ResourceVersion version) {
+        this.version = version;
+    }
+
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+        final FilterRegistrationBean<ShallowEtagHeaderFilter> filter = new FilterRegistrationBean<>(
+                new ShallowEtagHeaderFilter());
+        filter.addUrlPatterns("/etag", "/resources/" + version.getVersion() + "/js/index.js");
+        return filter;
+    }
 }
