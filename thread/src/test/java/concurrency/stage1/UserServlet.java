98917dd653524af2f257f32c18ba2599a7d90258
Jeong Jinwoo <jwjeong96@gmail.com>
2022-09-06 19:21:48 +0900

    test(study): check and analyze reason for concurrency issue

--- thread/src/test/java/concurrency/stage1/UserServlet.java
+++ thread/src/test/java/concurrency/stage1/UserServlet.java
@@ -13,7 +13,9 @@ public class UserServlet {
 
     private void join(final User user) {
         if (!users.contains(user)) {
-            users.add(user);
+            // 쓰레드A에서 users.add(user)가 호출되기 전인 경우, 쓰레드B도 여기에 도달!
+            // 평싱시에는 쓰레드A에서 join이 전부 실행된 이후에 쓰레드B가 join을 호출해서 문제 없던 것!
+            users.add(user); // Suspend: Thread 걸고 디버그로 테스트 실행시 fail!
         }
     }
 
