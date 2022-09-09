fc0e736edda39a65cc861079bd0378d0748156a2
Juhyung990122 <harukawa99@naver.com>
2022-09-09 15:21:02 +0900

    test: ThreadPoolsTest 완료

--- thread/src/test/java/concurrency/stage0/ThreadPoolsTest.java
+++ thread/src/test/java/concurrency/stage0/ThreadPoolsTest.java
@@ -25,14 +25,17 @@ class ThreadPoolsTest {
 
     @Test
     void testNewFixedThreadPool() {
+        //최대까지 만들고 재사용
         final var executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
         executor.submit(logWithSleep("hello fixed thread pools"));
         executor.submit(logWithSleep("hello fixed thread pools"));
         executor.submit(logWithSleep("hello fixed thread pools"));
 
         // 올바른 값으로 바꿔서 테스트를 통과시키자.
-        final int expectedPoolSize = 0;
-        final int expectedQueueSize = 0;
+        // 명령 3개, 스레드 풀에서 만들 수 있는 최대는 2개
+        final int expectedPoolSize = 2;
+        // 대기중인 명령은 1개
+        final int expectedQueueSize = 1;
 
         assertThat(expectedPoolSize).isEqualTo(executor.getPoolSize());
         assertThat(expectedQueueSize).isEqualTo(executor.getQueue().size());
@@ -40,13 +43,15 @@ class ThreadPoolsTest {
 
     @Test
     void testNewCachedThreadPool() {
+        // 안쓰는거 종료, 새로 들어오면 만듬. 풀사이즈는 제한 없음. 그냥 들어오는대로 만드는거임
+        // 이거 성능 이슈 없을까? 디도스 들어오면 스레드 와장창 생기느라 OOM 걸릴듯?
         final var executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
         executor.submit(logWithSleep("hello cached thread pools"));
         executor.submit(logWithSleep("hello cached thread pools"));
         executor.submit(logWithSleep("hello cached thread pools"));
 
         // 올바른 값으로 바꿔서 테스트를 통과시키자.
-        final int expectedPoolSize = 0;
+        final int expectedPoolSize = 3;
         final int expectedQueueSize = 0;
 
         assertThat(expectedPoolSize).isEqualTo(executor.getPoolSize());
