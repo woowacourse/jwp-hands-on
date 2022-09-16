40df282d3315b03cc2ce720030b016403df1d1ee
Kim Hyuntae <75936123+Hyunta@users.noreply.github.com>
2022-09-16 15:11:14 +0900

    Servlet 구현하기 (#3)

--- servlet/src/main/java/com/example/CharacterEncodingFilter.java
+++ servlet/src/main/java/com/example/CharacterEncodingFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 
 import java.io.IOException;
+import org.apache.tomcat.util.buf.Utf8Encoder;
 
 @WebFilter("/*")
 public class CharacterEncodingFilter implements Filter {
@@ -11,6 +12,9 @@ public class CharacterEncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         request.getServletContext().log("doFilter() 호출");
+
+        String characterEncoding = response.getCharacterEncoding();
+        response.setCharacterEncoding("UTF-8");
         chain.doFilter(request, response);
     }
 }
