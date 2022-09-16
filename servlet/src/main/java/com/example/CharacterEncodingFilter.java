1c70fd3bd1f9d3e20adf13e198af338f37440076
seong-wooo <seongwoo0513@uos.ac.kr>
2022-09-16 15:57:08 +0900

    test: 필터 학습 테스트 진행

--- servlet/src/main/java/com/example/CharacterEncodingFilter.java
+++ servlet/src/main/java/com/example/CharacterEncodingFilter.java
@@ -1,8 +1,11 @@
 package com.example;
 
-import jakarta.servlet.*;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
-
 import java.io.IOException;
 
 @WebFilter("/*")
@@ -11,6 +14,8 @@ public class CharacterEncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         request.getServletContext().log(getClass() + " doFilter() 호출");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/plain");
         chain.doFilter(request, response);
     }
 }
