0737114f0c655e089ea8e28d708f62f36348e3a5
woong7 <driedseas@gmail.com>
2022-09-16 15:47:11 +0900

    feat : 학습 테스트

--- servlet/src/main/java/com/example/CharacterEncodingFilter.java
+++ servlet/src/main/java/com/example/CharacterEncodingFilter.java
@@ -10,6 +10,7 @@ public class CharacterEncodingFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        response.setCharacterEncoding("UTF-8");
         request.getServletContext().log("doFilter() 호출");
         chain.doFilter(request, response);
     }
