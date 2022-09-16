2a7d7d6cd8890506ed17e040d30c372b558fac9d
Jeong Jinwoo <jwjeong96@gmail.com>
2022-09-16 15:19:19 +0900

    test(reflection): find classes with certain annotation

--- reflection/src/test/java/reflection/ReflectionsTest.java
+++ reflection/src/test/java/reflection/ReflectionsTest.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -9,10 +12,20 @@ class ReflectionsTest {
 
     private static final Logger log = LoggerFactory.getLogger(ReflectionsTest.class);
 
+    /**
+     * 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+     */
     @Test
-    void showAnnotationClass() throws Exception {
+    void showAnnotationClass() {
         Reflections reflections = new Reflections("examples");
-
-        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        for (final var type : reflections.getTypesAnnotatedWith(Controller.class)) {
+            log.info(type.toString());
+        }
+        for (final var type : reflections.getTypesAnnotatedWith(Service.class)) {
+            log.info(type.toString());
+        }
+        for (final var type : reflections.getTypesAnnotatedWith(Repository.class)) {
+            log.info(type.toString());
+        }
     }
 }
