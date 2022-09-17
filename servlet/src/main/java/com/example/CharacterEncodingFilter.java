0a30cd800e433529a5174d8209884ada58ec0d7b
dwl21 <nggus5@naver.com>
2022-09-17 16:27:49 +0900

    2단계 요구사항 완료

--- servlet/src/main/java/com/example/CharacterEncodingFilter.java
+++ servlet/src/main/java/com/example/CharacterEncodingFilter.java
@@ -1,9 +1,13 @@
 package com.example;
 
+import static org.eclipse.jdt.internal.compiler.util.Util.UTF_8;
+
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
 
 @WebFilter("/*")
 public class CharacterEncodingFilter implements Filter {
@@ -11,6 +15,7 @@ public class CharacterEncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         request.getServletContext().log("doFilter() 호출");
+        response.setCharacterEncoding(UTF_8);
         chain.doFilter(request, response);
     }
 }
