6978ca0054b92a338d4a9bccfef1292a2cadfaa8
Roma <52696169+kbsat@users.noreply.github.com>
2022-09-27 11:33:49 +0900

    Servlet 구현하기 (#3)
    * 1. servlet 학습 테스트
    
    * 2. 필터 학습 테스트

--- servlet/src/test/java/com/example/ServletTest.java
+++ servlet/src/test/java/com/example/ServletTest.java
@@ -25,7 +25,9 @@ class ServletTest {
 
         // expected를 0이 아닌 올바른 값으로 바꿔보자.
         // 예상한 결과가 나왔는가? 왜 이런 결과가 나왔을까?
-        assertThat(Integer.parseInt(response.body())).isEqualTo(0);
+        // - sahredCounter의 경우 인스턴스 변수로 가지고 있기 때문에 여러 쓰레드에서 하나의 서블릿을 함께 사용하기 때문에
+        // - 값이 여러 쓰레드에 의해 영향을 받게 되어 값이 계속 변경되게 된다.
+        assertThat(Integer.parseInt(response.body())).isEqualTo(3);
     }
 
     @Test
@@ -47,6 +49,8 @@ class ServletTest {
 
         // expected를 0이 아닌 올바른 값으로 바꿔보자.
         // 예상한 결과가 나왔는가? 왜 이런 결과가 나왔을까?
-        assertThat(Integer.parseInt(response.body())).isEqualTo(0);
+        // - 여기서는 localCounter를 사용한다. 하나의 init된 서블릿은 여러 쓰레드가 접근하여 사용하게 되는데 쓰레드에서
+        // - 로컬 변수는 다른 스레드와 공유되지 않는다.
+        assertThat(Integer.parseInt(response.body())).isEqualTo(1);
     }
 }
