a8c4eae20e61a4f22dd436e097552455370aaa23
Roma <52696169+kbsat@users.noreply.github.com>
2022-09-27 11:33:56 +0900

    자바 리플렉션 사용하기 (#4)

--- reflection/src/test/java/reflection/Junit4TestRunner.java
+++ reflection/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,9 @@
 package reflection;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -9,5 +13,20 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        final Constructor<Junit4Test> constructor = clazz.getConstructor();
+        final Junit4Test junit4Test = constructor.newInstance();
+
+        final Method[] declaredMethods = clazz.getDeclaredMethods();
+        for (final Method declaredMethod : declaredMethods) {
+            if (declaredMethod.isAnnotationPresent(MyTest.class)) { // isAnnotationPresent 이용
+                declaredMethod.invoke(junit4Test);
+            }
+        }
+
+//        for (Method declaredMethod : declaredMethods) {
+//            final Annotation[] annotations = declaredMethod.getAnnotations();
+//            Arrays.stream(annotations)
+//                    .forEach(it -> System.out.println(it.annotationType().getName()));
+//        }
     }
 }
--- reflection/src/test/java/reflection/ReflectionsTest.java
+++ reflection/src/test/java/reflection/ReflectionsTest.java
@@ -1,5 +1,9 @@
 package reflection;
 
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -14,5 +18,13 @@ class ReflectionsTest {
         Reflections reflections = new Reflections("examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        final Set<Class<?>> controllerAnnotatedWith = reflections.getTypesAnnotatedWith(Controller.class);
+        controllerAnnotatedWith.forEach(it -> System.out.println(it.getName()));
+
+        final Set<Class<?>> serviceAnnotatedWith = reflections.getTypesAnnotatedWith(Service.class);
+        serviceAnnotatedWith.forEach(it -> System.out.println(it.getName()));
+
+        final Set<Class<?>> repoAnnotatedWith = reflections.getTypesAnnotatedWith(Repository.class);
+        repoAnnotatedWith.forEach(it -> System.out.println(it.getName()));
     }
 }
