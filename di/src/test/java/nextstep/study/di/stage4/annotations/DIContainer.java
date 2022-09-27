ae4880ae1837666b65ccf335bdd0a875a9319107
yeon-06 <rnjstldus2@gmail.com>
2022-09-27 15:43:40 +0900

    feat: DIContainer에 bean 등록

--- di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
@@ -1,6 +1,10 @@
 package nextstep.study.di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -9,12 +13,29 @@ class DIContainer {
 
     private final Set<Object> beans;
 
-    public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+    private DIContainer(final Set<Class<?>> classes) {
+        this.beans = createBeans(classes);
+    }
+
+    private Set<Object> createBeans(final Set<Class<?>> classes) {
+        return classes.stream()
+                .map(this::createInstance)
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    private Object createInstance(final Class<?> clazz) {
+        try {
+            Constructor<?> constructor = clazz.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        return new DIContainer(ClassPathScanner.getAllClassesInPackage(rootPackageName));
     }
 
     @SuppressWarnings("unchecked")
