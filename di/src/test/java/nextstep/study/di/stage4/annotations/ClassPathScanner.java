ad2267593e9d581c3e76dc40591ee7a228dfb43c
yeon-06 <rnjstldus2@gmail.com>
2022-09-27 15:42:34 +0900

    feat: @Repository, @Service 어노테이션을 단 클래스들 모두 스캔

--- di/src/test/java/nextstep/study/di/stage4/annotations/ClassPathScanner.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,18 @@
 package nextstep.study.di.stage4.annotations;
 
+import java.util.HashSet;
 import java.util.Set;
+import org.reflections.Reflections;
 
 public class ClassPathScanner {
 
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        Reflections reflections = new Reflections(packageName);
+
+        Set<Class<?>> classes = new HashSet<>();
+        classes.addAll(reflections.getTypesAnnotatedWith(Repository.class));
+        classes.addAll(reflections.getTypesAnnotatedWith(Service.class));
+        
+        return classes;
     }
 }
