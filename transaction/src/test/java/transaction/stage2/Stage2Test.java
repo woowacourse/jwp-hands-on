c6819246040d50b28f334a853df39110f3dd04de
Jeong Jinwoo <jwjeong96@gmail.com>
2022-10-12 23:08:51 +0900

    test(transaction): check spring transaction propagation

--- transaction/src/test/java/transaction/stage2/Stage2Test.java
+++ transaction/src/test/java/transaction/stage2/Stage2Test.java
@@ -1,14 +1,14 @@
 package transaction.stage2;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * 트랜잭션 전파(Transaction Propagation)란?
@@ -36,8 +36,9 @@ class Stage2Test {
     }
 
     /**
-     * 생성된 트랜잭션이 몇 개인가?
+     * 생성된 트랜잭션이 몇 개인가? 1개
      * 왜 그런 결과가 나왔을까?
+     * REQUIRED 내부에서 REQUIRED 호출 - 통합됨
      */
     @Test
     void testRequired() {
@@ -45,13 +46,14 @@ class Stage2Test {
 
         log.info("transactions : {}", actual);
         assertThat(actual)
-                .hasSize(0)
-                .containsExactly("");
+                .hasSize(1)
+                .containsExactlyInAnyOrder("transaction.stage2.FirstUserService.saveFirstTransactionWithRequired");
     }
 
     /**
-     * 생성된 트랜잭션이 몇 개인가?
+     * 생성된 트랜잭션이 몇 개인가? 2개
      * 왜 그런 결과가 나왔을까?
+     * REQUIRED 내부에서 REQUIRES_NEW 호출 - 별도의 트랜잭션을 내부에서 생성
      */
     @Test
     void testRequiredNew() {
@@ -59,27 +61,35 @@ class Stage2Test {
 
         log.info("transactions : {}", actual);
         assertThat(actual)
-                .hasSize(0)
-                .containsExactly("");
+                .hasSize(2)
+                .containsExactlyInAnyOrder("transaction.stage2.FirstUserService.saveFirstTransactionWithRequiredNew",
+                    "transaction.stage2.SecondUserService.saveSecondTransactionWithRequiresNew");
     }
 
     /**
      * firstUserService.saveAndExceptionWithRequiredNew()에서 강제로 예외를 발생시킨다.
      * REQUIRES_NEW 일 때 예외로 인한 롤백이 발생하면서 어떤 상황이 발생하는 지 확인해보자.
+     *
+     * REQUIRED 내부에서 REQUIRES_NEW 호출
+     * - 별도의 트랜잭션을 내부에서 생성하여 커밋한 이후에 기존 트랜잭션이 롤백되는 경우
+     * - REQUIRES_NEW 트랜잭션의 결과는 반영됨
      */
     @Test
     void testRequiredNewWithRollback() {
-        assertThat(firstUserService.findAll()).hasSize(-1);
+        assertThat(firstUserService.findAll()).hasSize(0);
 
         assertThatThrownBy(() -> firstUserService.saveAndExceptionWithRequiredNew())
                 .isInstanceOf(RuntimeException.class);
 
-        assertThat(firstUserService.findAll()).hasSize(-1);
+        assertThat(firstUserService.findAll()).hasSize(1);
     }
 
     /**
      * FirstUserService.saveFirstTransactionWithSupports() 메서드를 보면 @Transactional이 주석으로 되어 있다.
      * 주석인 상태에서 테스트를 실행했을 때와 주석을 해제하고 테스트를 실행했을 때 어떤 차이점이 있는지 확인해보자.
+     *
+     * 트랜잭션이 아닌 곳에서 내부적으로 SUPPORTS 호출: SUPPORTS에서 새로운 트랜잭션 생성 - saveSecondTransactionWithSupports
+     * REQUIRED 트랜잭션 내부에서 SUPPORTS 호출: REQUIRED 트랜잭션에 통합 - saveFirstTransactionWithSupports
      */
     @Test
     void testSupports() {
@@ -87,14 +97,17 @@ class Stage2Test {
 
         log.info("transactions : {}", actual);
         assertThat(actual)
-                .hasSize(0)
-                .containsExactly("");
+                .hasSize(1)
+                .containsExactlyInAnyOrder("transaction.stage2.FirstUserService.saveFirstTransactionWithSupports");
     }
 
     /**
      * FirstUserService.saveFirstTransactionWithMandatory() 메서드를 보면 @Transactional이 주석으로 되어 있다.
      * 주석인 상태에서 테스트를 실행했을 때와 주석을 해제하고 테스트를 실행했을 때 어떤 차이점이 있는지 확인해보자.
      * SUPPORTS와 어떤 점이 다른지도 같이 챙겨보자.
+     *
+     * 트랜잭션 내부가 아닌 곳에서 MANDATORY 호출한 경우 예외 발생!
+     * REQUIRED 트랜잭션 내부에서 MANDATORY 호출: REQUIRED 트랜잭션에 통합 - saveFirstTransactionWithMandatory
      */
     @Test
     void testMandatory() {
@@ -102,8 +115,8 @@ class Stage2Test {
 
         log.info("transactions : {}", actual);
         assertThat(actual)
-                .hasSize(0)
-                .containsExactly("");
+                .hasSize(1)
+                .containsExactlyInAnyOrder("transaction.stage2.FirstUserService.saveFirstTransactionWithMandatory");
     }
 
     /**
@@ -112,6 +125,12 @@ class Stage2Test {
      * 다시 테스트를 실행하면 몇 개의 물리적 트랜잭션이 동작할까?
      *
      * 스프링 공식 문서에서 물리적 트랜잭션과 논리적 트랜잭션의 차이점이 무엇인지 찾아보자.
+     *
+     * 트랜잭션이 아닌 곳에서 내부적으로 NOT_SUPPORTED 호출
+     * - [saveSecondTransactionWithNotSupported]
+     *
+     * REQUIRED 트랜잭션 내부에서 NOT_SUPPORTED 호출
+     * - [saveFirstTransactionWithNotSupported, saveSecondTransactionWithNotSupported]
      */
     @Test
     void testNotSupported() {
@@ -119,13 +138,16 @@ class Stage2Test {
 
         log.info("transactions : {}", actual);
         assertThat(actual)
-                .hasSize(0)
-                .containsExactly("");
+                .hasSize(2)
+                .containsExactlyInAnyOrder("transaction.stage2.FirstUserService.saveFirstTransactionWithNotSupported",
+                    "transaction.stage2.SecondUserService.saveSecondTransactionWithNotSupported");
     }
 
     /**
-     * 아래 테스트는 왜 실패할까?
-     * FirstUserService.saveFirstTransactionWithNested() 메서드의 @Transactional을 주석 처리하면 어떻게 될까?
+     * REQUIRED 내부에서 NESTED 호출시, 예외 발생
+     * - JpaDialect does not support savepoints
+     *
+     * 트랜잭션 아닌 곳에서 내부적으로 NESTED 호출시 REQUIRED처럼 동작 - 내부적으로 트랜잭션 생성
      */
     @Test
     void testNested() {
@@ -133,12 +155,15 @@ class Stage2Test {
 
         log.info("transactions : {}", actual);
         assertThat(actual)
-                .hasSize(0)
-                .containsExactly("");
+                .hasSize(1)
+                .containsExactlyInAnyOrder("transaction.stage2.SecondUserService.saveSecondTransactionWithNested");
     }
 
     /**
-     * 마찬가지로 @Transactional을 주석처리하면서 관찰해보자.
+     * 트랜잭션 내부에서 NEVER 호출시, 예외 발생
+     * - Existing transaction found for transaction marked with propagation 'never'
+     *
+     * 트랜잭션 아닌 곳에서 내부적으로 NEVER 호출시 REQUIRED처럼 동작 - 내부적으로 트랜잭션 생성
      */
     @Test
     void testNever() {
@@ -146,7 +171,7 @@ class Stage2Test {
 
         log.info("transactions : {}", actual);
         assertThat(actual)
-                .hasSize(0)
-                .containsExactly("");
+                .hasSize(1)
+                .containsExactlyInAnyOrder("transaction.stage2.SecondUserService.saveSecondTransactionWithNever");
     }
 }
