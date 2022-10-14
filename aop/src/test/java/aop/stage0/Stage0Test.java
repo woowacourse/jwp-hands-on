719a09ef0bfd0db8907a44d7ae4cc71b63ca1406
Jeong Jinwoo <jwjeong96@gmail.com>
2022-10-14 16:48:56 +0900

    test(aop): implement JDK Proxy with InvocationHandler

--- aop/src/test/java/aop/stage0/Stage0Test.java
+++ aop/src/test/java/aop/stage0/Stage0Test.java
@@ -1,5 +1,9 @@
 package aop.stage0;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
 import aop.DataAccessException;
 import aop.StubUserHistoryDao;
 import aop.domain.User;
@@ -7,6 +11,8 @@ import aop.repository.UserDao;
 import aop.repository.UserHistoryDao;
 import aop.service.AppUserService;
 import aop.service.UserService;
+import java.lang.reflect.Proxy;
+import javax.sql.DataSource;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -15,14 +21,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class Stage0Test {
 
     private static final Logger log = LoggerFactory.getLogger(Stage0Test.class);
+
+    @Autowired
+    private DataSource dataSource;
 
     @Autowired
     private UserDao userDao;
@@ -42,10 +47,17 @@ class Stage0Test {
         userDao.insert(user);
     }
 
+    /**
+     * [Baeldung - Dynamic Proxies in Java](https://www.baeldung.com/java-dynamic-proxies)
+     * <p>
+     * JDK Proxy: Java Dynamic Proxy
+     */
     @Test
     void testChangePassword() {
         final var appUserService = new AppUserService(userDao, userHistoryDao);
-        final UserService userService = null;
+        final UserService userService = (UserService) Proxy.newProxyInstance(
+            Stage0Test.class.getClassLoader(), new Class<?>[]{UserService.class},
+            new TransactionHandler(appUserService, dataSource));
 
         final var newPassword = "qqqqq";
         final var createBy = "gugu";
@@ -59,12 +71,14 @@ class Stage0Test {
     @Test
     void testTransactionRollback() {
         final var appUserService = new AppUserService(userDao, stubUserHistoryDao);
-        final UserService userService = null;
+        final UserService userService = (UserService) Proxy.newProxyInstance(
+            Stage0Test.class.getClassLoader(), new Class<?>[]{UserService.class},
+            new TransactionHandler(appUserService, dataSource));
 
         final var newPassword = "newPassword";
         final var createBy = "gugu";
         assertThrows(DataAccessException.class,
-                () -> userService.changePassword(1L, newPassword, createBy));
+            () -> userService.changePassword(1L, newPassword, createBy));
 
         final var actual = userService.findById(1L);
 
--- aop/src/test/java/aop/stage0/TransactionHandler.java
+++ aop/src/test/java/aop/stage0/TransactionHandler.java
@@ -1,15 +1,60 @@
 package aop.stage0;
 
+import aop.DataAccessException;
+import aop.Transactional;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import javax.sql.DataSource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
+/**
+ * InvocationHandler is the interface implemented by the invocation handler of a proxy instance. Each proxy instance has
+ * an associated invocation handler. When a method is invoked on a proxy instance, the method invocation is encoded and
+ * dispatched to the invoke method of its invocation handler.
+ */
 public class TransactionHandler implements InvocationHandler {
+
+    private final Object target;
+    private final DataSource dataSource;
+
+    public TransactionHandler(Object target, DataSource dataSource) {
+        this.target = target;
+        this.dataSource = dataSource;
+    }
 
     /**
      * @Transactional 어노테이션이 존재하는 메서드만 트랜잭션 기능을 적용하도록 만들어보자.
      */
     @Override
     public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
-        return null;
+        Method instanceMethod = getActualMethod(method);
+        if (!instanceMethod.isAnnotationPresent(Transactional.class)) {
+            return instanceMethod.invoke(target, args);
+        }
+        return executeTransaction(args, instanceMethod);
+    }
+
+    private Object executeTransaction(Object[] args, Method instanceMethod) {
+        PlatformTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
+        final var transactionStatus = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            Object result = instanceMethod.invoke(target, args);
+            transactionManager.commit(transactionStatus);
+            return result;
+        } catch (Exception e) {
+            transactionManager.rollback(transactionStatus);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private Method getActualMethod(Method interfaceMethod) {
+        return Arrays.stream(target.getClass().getMethods())
+            .filter(it -> it.getName().equals(interfaceMethod.getName()))
+            .filter(it -> Arrays.equals(it.getParameterTypes(), interfaceMethod.getParameterTypes()))
+            .findAny()
+            .orElseThrow(() -> new RuntimeException("등록된 구현체에 존재하지 않는 메서드를 호출하였습니다."));
     }
 }
