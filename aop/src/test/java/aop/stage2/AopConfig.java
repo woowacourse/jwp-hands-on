0a5b95723cb5a2a9428736507d4105d917b2ff63
Jeong Jinwoo <jwjeong96@gmail.com>
2022-10-16 15:40:33 +0900

    feat(aop): config Transaction AOP as Beans

--- aop/src/test/java/aop/stage2/AopConfig.java
+++ aop/src/test/java/aop/stage2/AopConfig.java
@@ -1,8 +1,38 @@
 package aop.stage2;
 
+import aop.stage1.TransactionAdvice;
+import aop.stage1.TransactionAdvisor;
+import aop.stage1.TransactionPointcut;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class AopConfig {
 
+    @Bean
+    public TransactionPointcut transactionPointcut() {
+        return new TransactionPointcut();
+    }
+
+    @Bean
+    public TransactionAdvice transactionAdvice(final PlatformTransactionManager platformTransactionManager) {
+        return new TransactionAdvice(platformTransactionManager);
+    }
+
+    @Bean
+    public TransactionAdvisor transactionAdvisor(final TransactionPointcut transactionPointcut,
+                                                 final TransactionAdvice transactionAdvice) {
+        return new TransactionAdvisor(transactionPointcut, transactionAdvice);
+    }
+
+    /**
+     * DefaultAdvisorAutoProxyCreator: BeanPostProcessor implementation that creates AOP proxies based on all candidate
+     * Advisors in the current BeanFactory
+     */
+    @Bean
+    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+        return new DefaultAdvisorAutoProxyCreator();
+    }
 }
